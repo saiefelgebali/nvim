e88fac49ee69bcfc5c75378b9d8be404346f2dca
Seif Ahmed <seif.ahmed@softwire.com>
2024-05-03 10:24:28 +0100

    Use tab for auto-complete

--- lua/saiefelgebali/plugins/nvim-cmp.lua
+++ lua/saiefelgebali/plugins/nvim-cmp.lua
@@ -41,6 +41,7 @@ return {
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
                 ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+                ["<Tab>"] = cmp.mapping.confirm({ select = true }),
                 ["<CR>"] = cmp.mapping.confirm({ select = false }),
             }),
 
