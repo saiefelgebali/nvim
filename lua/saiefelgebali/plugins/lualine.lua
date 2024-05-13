5e147b2938c27d0616df12dedd565113fd291e9d
Seif Ahmed <seif.ahmed@softwire.com>
2024-05-13 09:39:39 +0100

    Add file path to lualine

--- lua/saiefelgebali/plugins/lualine.lua
+++ lua/saiefelgebali/plugins/lualine.lua
@@ -4,6 +4,22 @@ return {
     config = function()
         local lualine = require("lualine")
 
-        lualine.setup({})
+        lualine.setup({
+            sections = {
+                lualine_c = {
+                    {
+                        "filename",
+                        path = 1,
+                    },
+                },
+                lualine_x = {
+                    {
+                        require("noice").api.statusline.mode.get,
+                        cond = require("noice").api.statusline.mode.has,
+                        color = { fg = "#ff9e64" },
+                    },
+                },
+            },
+        })
     end,
 }
