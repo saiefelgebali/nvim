780086c8f43c8f83e0fd5ebfd491ddc12c7d131d
Seif Ahmed <seif.ahmed@softwire.com>
2025-06-11 11:48:53 +0100

    Add pre-alpha kotlin-ls config

--- lua/saiefelgebali/plugins/lsp/lspconfig.lua
+++ lua/saiefelgebali/plugins/lsp/lspconfig.lua
@@ -168,5 +168,20 @@ return {
                 lspconfig.mdx_analyzer.setup({})
             end,
         })
+
+        local configs = require("lspconfig.configs")
+
+        if not configs.kotlin_ls then
+            configs.kotlin_ls = {
+                default_config = {
+                    cmd = { "kotlin-ls", "--stdio" },
+                    single_file_support = true,
+                    filetypes = { "kotlin" },
+                    root_markers = { "build.gradle", "build.gradle.kts", "pom.xml" },
+                },
+            }
+        end
+
+        lspconfig.kotlin_ls.setup({})
     end,
 }
