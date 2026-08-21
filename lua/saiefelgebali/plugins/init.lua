3767b72fda39d4216c5139de1b8975f846144487
Seif Ahmed <saiefelsayed@gmail.com>
2026-08-21 12:22:57 +0100

    feat: Add support for herdr

--- lua/saiefelgebali/plugins/init.lua
+++ lua/saiefelgebali/plugins/init.lua
@@ -1,4 +1,16 @@
 return {
-    "christoomey/vim-tmux-navigator",
+    {
+        "christoomey/vim-tmux-navigator",
+        dependencies = { "paulbkim-dev/vim-herdr-navigation" },
+        lazy = false,
+        init = function()
+            -- Let vim-herdr-navigation own <C-hjkl>. Its editor script falls
+            -- back to TmuxNavigate* when inside tmux, so tmux keeps working.
+            vim.g.tmux_navigator_no_mappings = 1
+        end,
+        config = function()
+            dofile(vim.fn.stdpath("data") .. "/lazy/vim-herdr-navigation/editor/nvim.lua")
+        end,
+    },
     "gpanders/editorconfig.nvim",
 }
